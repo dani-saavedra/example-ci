8ea6f1b4e45fb95a9266da24331a3a494ca37b57
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2025-03-31 20:00:14 -0500

    Prueba

---
+++ src/test/java/co/edu/unisabana/exampleci/service/LogicaTest.java
@@ -0,0 +1,12 @@
+package co.edu.unisabana.exampleci.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogicaTest {
+
+    @Test
+    void sumar() {
+    }
+}
