cf8f8148d5ad4ac921408cf3618eac3be3589b9c
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2025-03-31 19:16:23 -0500

    Initial commit

---
+++ src/main/java/co/edu/unisabana/exampleci/ExampleciApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.exampleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleciApplication.class, args);
+	}
+
+}
